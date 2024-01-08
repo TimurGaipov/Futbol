fe7b283f24b6b13d16c06d1198c55eb448fdf30d
Spider748 <gaipovt2@gmail.com>
2024-01-08 20:13:21 +0500

    init

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import NavBar from "./components/navbar";
+import { fetchFixtures } from "./lib/fetch-data";
+import { data } from "./lib/dummy-data";
+import Table from "./components/table";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Fixture from "./components/fixture";
+import CircularProgress from "@mui/material/CircularProgress";
+import Box from "@mui/material/Box";
+import CachedIcon from "@mui/icons-material/Cached";
+
+function App() {
+  const [fixtures, setFixtures] = useState(data);
+  console.log(fixtures);
+
+  const refresh = () => window.location.reload(true);
+
+  return (
+    <div className="w-full md:w-[700px]  lg:w-[800px] m-auto">
+      <NavBar />
+
+      <button
+        onClick={refresh}
+        className="btn btn-sm fixed bottom-3 right-2 z-40"
+      >
+        <CachedIcon />
+      </button>
+
+      {fixtures.length == 0 ? (
+        <div className="h-screen bg-white w-full text-center p-10">
+          <Box>
+            <CircularProgress />
+          </Box>
+        </div>
+      ) : (
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Table data={fixtures} />}></Route>
+            <Route
+              path="/fixture/:matchID"
+              element={<Fixture data={fixtures} />}
+            ></Route>
+          </Routes>
+        </BrowserRouter>
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/fixture.js
@@ -0,0 +1,126 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import BALLIMG from "../assets/images/1.png";
+import { fetchFixtures } from "../lib/fetch-data";
+import { data } from "../lib/dummy-data";
+
+export default function Fixture() {
+
+  const params = useParams();
+  const matchID = params.matchID;
+
+  const results = data.response.filter((match) => {
+    console.log(match);
+    return match.fixture.id == matchID;
+  });
+
+  const fixture = results[0];
+
+  return (
+    <div className="pb-10 bg-white">
+      <div key={fixture.fixture.id} className="bg-white py-2">
+        <div align="center">
+          <img src={fixture.league.logo} width={25} alt="logo" />
+          {fixture.league.name}
+        </div>
+
+        <div className="w-full flex p-1">
+          <div className="w-[10%]" align="center">
+            <img src={fixture.teams.home.logo} width={30} />
+          </div>
+
+          <div className="w-[32%] text-right">{fixture.teams.home.name}</div>
+
+          <div className="w-[16%] text-center">
+            {fixture.goals.home} : {fixture.goals.away}
+          </div>
+
+          <div className="w-[32%] text-left flex">
+            {fixture.teams.away.name}
+          </div>
+
+          <div className="w-[10%]" align="center">
+            <img src={fixture.teams.away.logo} width={30} />
+          </div>
+        </div>
+
+        <div className="text-center text-green-600">
+          {fixture.fixture.status.elapsed}`
+        </div>
+      </div>
+
+      <div align="center" className="grid grid-cols-1 divide-y">
+        <h1 className=" bg-red-700 p-1 text-white text-xl">События</h1>
+
+        {!fixture.events
+          ? null
+          : fixture.events.map((event) => (
+              <div className="p-5" key={event.team.id}>
+                {event.type === "Goal" ? (
+                  <div>
+                    <img src={BALLIMG} width={15} />
+                  </div>
+                ) : (
+                  <div className="badge badge-secondary">{event.type}</div>
+                )}{" "}
+                {event.player.name}{" "}
+                <img src={event.team.logo} width={20} />
+                <br />
+                <div className="text-green-700">{event.time.elapsed}</div>
+              </div>
+            ))}
+      </div>
+
+      <div align="center" className="grid grid-cols-1 divide-y">
+        <h1 className=" bg-red-700 p-1 text-white text-xl">Счёт</h1>
+
+        <div className="p-2">
+          Первая половина
+          <br />
+          {fixture.score.halftime.home} : {fixture.score.halftime.away}
+        </div>
+
+        {fixture.score.fulltime.home ? (
+          <div className="p-2">
+            Полное время
+            <br />
+            {fixture.score.fulltime.home} : {fixture.score.fulltime.away}
+          </div>
+        ) : null}
+
+        {fixture.score.extratime.home ? (
+          <div className="p-2">
+            Дополнительное время
+            <br />
+            {fixture.score.extratime.home} : {fixture.score.extratime.away}
+          </div>
+        ) : null}
+
+        {fixture.score.penalty.home ? (
+          <div className="p-2">
+            Дополнительное время
+            <br />
+            {fixture.score.penalty.home} : {fixture.score.penalty.away}
+          </div>
+        ) : null}
+      </div>
+
+      <div align="center" className="grid grid-cols-1 divide-y">
+        <h1 className=" bg-red-700 p-1 text-white text-xl">
+          Детали матча
+        </h1>
+
+        <div className="p-2">Стадион - {fixture.fixture.venue.name}</div>
+
+        <div className="p-2">Судья - {fixture.fixture.referee}</div>
+        <div className="p-2">Страна - {fixture.league.country}</div>
+        <div className="p-2">{fixture.league.round}</div>
+        <div className="p-2">{fixture.league.season}</div>
+      </div>
+
+      <div className="text-center">
+        <button className="btn btn-wide bg-red-700 text-white">Оплатить за Live</button>
+      </div>
+    </div>
+  );
+}
